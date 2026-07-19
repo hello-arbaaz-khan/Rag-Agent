0cf8d3110d602930a90885862bd9b189361685b3
Arbaz Khan <hello.arbaaz.khan@gmail.com>
2026-07-19 22:29:08 +0500

    Initail commit

--- frontend/src/components/Common/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-const Spinner = ({ className = "h-4 w-4" }) => (
-  <span
-    className={`${className} inline-block animate-spin rounded-full border-2 border-current border-r-transparent`}
-    aria-label="Loading"
-  />
-);
-
-export default Spinner;
